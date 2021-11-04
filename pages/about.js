de1cce321490da4367d1d395440de4087489691d
gmwill512 <gmarvwill413@gmail.com>
2021-11-04 13:08:43 -0500

    Final Commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function About() {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h1>Something else</h1>
+    </div>
+  );
+}
+
+export default About;
